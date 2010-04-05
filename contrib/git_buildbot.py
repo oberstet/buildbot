7ae6b064869fcb0a4134353c3767d39099f1918e
Tobias Oberstein <tobias.oberstein@tavendo.de>
2010-04-05 18:04:13 +0200

    Add various options to control the change sent to BuildBot and provide feedback.
    * flexible options for controlling repository sent
        --repository    : provide fully qualified Git repo URL (will override the following)
        --git-host      : provide Git repo host, will get concatenated with CWD to
                          sent fully qualified repo URL
        no options used : will just send CWD (local Git repo filesystem path) as repository,
                          still allowing to construct full qualified repo URL on BuildBot side
    
    * --project option : sent project string
    
    * --filter-commits : control via commit message iff commit is sent as change, what category is sent
                         and properties sent
    
    * feedback: when BuildBot sends back change (see other patch), provide change # and change details
    
    * feedback: when BuildBot sends back change web status URL (see other patch), provide URL for user
                to track change in browser

--- contrib/git_buildbot.py
+++ contrib/git_buildbot.py
@@ -28,6 +28,7 @@ import logging
 import os
 import re
 import sys
+import pprint
 
 from twisted.spread import pb
 from twisted.cred import credentials
@@ -40,6 +41,11 @@ from optparse import OptionParser
 
 master = "localhost:9989"
 
+# When sending the notification, send this project iff
+# it's set (via --project)
+
+project = None
+
 # When sending the notification, send this category iff
 # it's set (via --category)
 
@@ -49,6 +55,12 @@ category = None
 # it's set (via --repository)
 
 repository = None
+
+# When sending the notification, send this this Git host string
+# concatenated with current working dir (local filesystem path of Git repo)
+# as repository (set via --git-host, overridded by --repository)
+
+git_host = None
 
 # When converting strings to unicode, assume this encoding. 
 # (set with --encoding)
@@ -60,6 +72,39 @@ encoding = 'utf8'
 # installed in.
 
 changes = []
+repository_sent = None
+
+#
+# Here is an example commit message:
+#
+# "Some change (buildbot full {'style': 'frantic', 'use_threads': 4 * 4})"
+# 
+# This message will make the change be sent to BuidlBot master with category 'quick'
+# and the properties igven between curly braces.
+#
+filter_commits = False
+
+buildbot_commit_msg = re.compile ('.*\(\s*buildbot\s*(?P<category>[^\s{\)]+)?\s*(?P<properties>{[^}]*})?\)\s*$', re.DOTALL | re.IGNORECASE)
+
+def parse_buildbot_commit_msg (msg):
+   mat = buildbot_commit_msg.match (msg)
+   if mat is not None:
+      catstr = mat.group ('category')
+      if catstr is not None:
+         cat = catstr.strip ()
+      else:
+         cat = None
+      propstr = mat.group ('properties')
+      try:
+         props = eval (propstr)
+      except:
+         print "could not evaluate build properties string : %s"% propstr
+         print "will use empty property bag instead!"
+         props = None
+      return True, cat, props
+   else:
+      return False, None, None
+
 
 def connectFailed(error):
     logging.error("Could not connect to %s: %s"
@@ -84,7 +129,14 @@ def addChanges(remote, changei):
             d = addChange(c)
             # handle successful completion by re-iterating, but not immediately
             # as that will blow out the Python stack
-            def cb(_):
+            def cb(res):
+                if res is not None:
+                    print "Change #%d received by BuildBot." % res['number']
+                    if res.has_key('change_web_status_url'):
+                        print "BuildBot change web status URL: ", res['change_web_status_url']
+                    pprint.pprint (res)
+                else:
+                    print "Change received by BuildBot."
                 reactor.callLater(0, iter)
             d.addCallback(cb)
             # and pass errors along to the outer deferred
@@ -146,14 +198,27 @@ def gen_changes(input, branch):
              'branch': unicode(branch, encoding=encoding),
         }
 
-        if category:
-            c['category'] = unicode(category, encoding=encoding)
+        if filter_commits:
+            sendchg, cat, props = parse_buildbot_commit_msg (m.group (2))
+        else:
+            sendchg, cat, props = True, None, None
 
-        if repository:
-            c['repository'] = unicode(repository, encoding=encoding)
+        if sendchg:
 
-        grab_commit_info(c, m.group(1))
-        changes.append(c)
+            if category or cat:
+                c['category'] = unicode(category or cat, encoding=encoding)
+
+            if project:
+                c['project'] = unicode(project, encoding=encoding)
+
+            if repository_sent:
+                c['repository'] = unicode(repository_sent, encoding=encoding)
+            
+            if props:
+                c['properties'] = props
+            
+            grab_commit_info(c, m.group(1))
+            changes.append(c)
 
 
 def gen_create_branch_changes(newrev, refname, branch):
@@ -217,8 +282,11 @@ def gen_update_branch_changes(oldrev, newrev, refname, branch):
         if category:
             c['category'] = unicode(category, encoding=encoding)
 
-        if repository:
-            c['repository'] = unicode(repository, encoding=encoding)
+        if project:
+            c['project'] = unicode(project, encoding=encoding)
+
+        if repository_sent:
+            c['repository'] = unicode(repository_sent, encoding=encoding)
 
         if files:
             c['files'] = files
@@ -295,10 +363,34 @@ def parse_options():
                    { 'master' : master })
     parser.add_option("-m", "--master", action="store", type="string",
             help=master_help)
+    filter_commits_help = """Parse commit message to control forwarding changes to BuildBot.
+Default is off. When turned on, changes will only be forwarded to BuildBot when the commit message ends with the string "(buildbot)".
+The keyword "buildbot" is case-insensitive, and trailing whitespace after the closing ) is allowed.
+You can control the category and/or properties sent using this format: "(buildbot <category> <Python dictionary with properties>)".
+For example: "Some change (buildbot full {'style': 'frantic', 'use_threads': 4 * 4})") will send category=full and properties.
+If --category is used also, category from parsed commit message will be ignored.
+"""
+    parser.add_option("-f", "--filter-commits", action="store_true",
+                      dest="filter_commits", default=False,
+                      help=filter_commits_help)
     parser.add_option("-c", "--category", action="store",
-                      type="string", help="Scheduler category to notify.")
+                      type="string", help="Send this category string on any change sent to BuildBot. "
+                      "Takes precedence over any category given in commit message, when --filter-commits option is used.")
+    parser.add_option("-p", "--project", action="store",
+                      type="string", help="Send this project string on any change sent to BuildBot.")
     parser.add_option("-r", "--repository", action="store",
-                      type="string", help="Git repository URL to send.")
+                      type="string", help="Git repository URL to send with change. "
+                      "You should use a fully qualified Git repository URL. "
+                      "This option is useful if you send changes from multiple Git repositories to a single build master. "
+                      "This parameter takes precedence over --git-host. If neither --repository, nor --git-host is used, "
+                      "the sent repository will just be the current working dir (the local filesystem path of the Git repo).")
+    parser.add_option("-g", "--git-host", action="store",
+                      type="string", help="Git repository host. If this option is given (and --repository is not), the hook "
+                      "will concatenate this string with the current working directory (the local filesystem path of the Git repo) "
+                      "and send the resulting string (the fully qualified Git repo URL) as repository parameter with the change to BuildBot. "
+                      "This option is useful if you send changes from multiple Git repositories to a single build master. "
+                      "If --repository is used also, --git-host will be ignored. If neither --git-host, nor --repository is used, "
+                      "the sent repository will just be the current working dir (the local filesystem path of the Git repo).")
     encoding_help = ("Encoding to use when converting strings to "
                      "unicode. Default is %(encoding)s." % 
                      { "encoding" : encoding })
@@ -335,11 +427,28 @@ try:
     if options.master:
         master=options.master
 
+    if options.filter_commits:
+        filter_commits = True
+
     if options.category:
         category = options.category
 
+    if options.project:
+        project = options.project
+
     if options.repository:
         repository = options.repository
+
+    if options.git_host:
+        git_host = options.git_host
+
+    if repository is None:
+       if git_host is not None:
+          repository_sent = git_host + os.getcwd ()
+       else:
+          repository_sent = os.getcwd ()
+    else:
+        repository_sent = repository
 
     if options.encoding:
         encoding = options.encoding
